6af6bba2fa0537d5ede35d8176d53979971aacbf
bitvalser <bitvalser@gmail.com>
2019-01-26 13:19:03 +0300

    bugfix

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ const parseMessages = url => {
             content: $(`#${message.attribs.id} .messageContent`)
               .html()
               .trim()
-              .replace(new RegExp('[\ts\n]', 'g'), ''),
+              .replace(new RegExp('[\t\s\n]', 'g'), ''),
             number: $(`#${message.attribs.id} .publicControls a`).text(),
             likes: $(`#${message.attribs.id} .LikeText`)
               .text()
